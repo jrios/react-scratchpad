461b2a42dede088a507614c1abbcd71de2711ead
Jake Rios <rios.jacob@gmail.com>
2014-07-11 11:01:21 -0600

    Update sample-2-with-jsx.js
    Make this sample a bit more simplistic

--- sample-2-with-jsx.js
+++ sample-2-with-jsx.js
@@ -4,20 +4,8 @@ var React = require("react");
 
 var Item = React.createClass({
   render: function() {
-    return <li>{ this.props.itemName }</li>;
+    return <h1>Hello from JSX!</h1>;
   }
 });
 
-var ItemList = React.createClass({
-  render: function() {
-    var items = this.props.items.map(function(item) {
-      return <Item itemName={item} />;
-    });
-
-    return <ul>{ items }</ul>;
-  }
-});
-
-var items = ["Item 1", "Item 2", "Item 3"];
-
-React.renderComponent(<ItemList items={items} />, document.body);
+React.renderComponent(<Item />, document.body);
