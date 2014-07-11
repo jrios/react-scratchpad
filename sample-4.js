94956f6a6061d0b0b11779115c17539df547d744
Jake Rios <rios.jacob@gmail.com>
2014-07-11 11:02:07 -0600

    Update sample-4.js
    Get rid of Cart in this sample

--- sample-4.js
+++ sample-4.js
@@ -31,23 +31,4 @@ var ProductList = React.createClass({
   }
 });
 
-var Cart = React.createClass({
-  getInitialState: function() {
-    return {
-      cartItems: []
-    }
-  },
-  render: function() {
-    var cartItems = this.state.cartItems.map(function(cartItem) {
-      return <CartItem item={cartItem.name} quantity={cartItem.quantity} />
-    });
-    return (
-      <div>
-        <h3>Shopping Cart<h3>}
-        { cartItems }
-      </div>
-    )
-  }
-});
-
 React.renderComponent(<ProductList />, document.body);
