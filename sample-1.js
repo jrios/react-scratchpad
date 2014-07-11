888477d169b1b2616d216918b355342eefe485b6
Jake Rios <rios.jacob@gmail.com>
2014-07-11 11:00:14 -0600

    Update sample-1.js
    Made it a bit more simple

--- sample-1.js
+++ sample-1.js
@@ -2,19 +2,8 @@ var React = require("react");
 
 var Item = React.createClass({
     render: function() {
-        return React.DOM.li(null, this.props.itemName);
+        return React.DOM.h1(null, "Hello!");
     } 
 });
 
-var ItemList = React.createClass({
-    render: function() {
-        var items = this.props.items.map(function(item) {
-          return Item({ itemName: item });
-        });
-        return React.DOM.ul({className: "items"}, items);
-    }
-});
-
-var items = ["Item 1", "Item 2", "Item 3"];
-
-React.renderComponent(ItemList({ items: items}), document.body);
+React.renderComponent(Item(), document.body);
